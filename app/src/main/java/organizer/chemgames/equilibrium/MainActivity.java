0d09b0cf368be1324a4895c7524e26f99793a864
Olgmuth <poolmuth@gmail.com>
2018-12-11 17:08:05 +0100

    basic timer

--- app/src/main/java/organizer/chemgames/equilibrium/MainActivity.java
+++ app/src/main/java/organizer/chemgames/equilibrium/MainActivity.java
@@ -3,6 +3,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -11,12 +12,14 @@ import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -26,307 +29,60 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    // 7 days in milliseconds - 7 * 24 * 60 * 60 * 1000
-    private static final int SEVEN_DAYS = 604800000;
+    TextView progres;
+    TextView somedata;
+    ProgressBar progressBar;
+    Thread mThread;
+    Task myTask;
+    Timer myTimer;
 
-    private static String timeString;
-    private static String dateString;
-    private static TextView dateView;
-    private static TextView timeView;
-
-    private Date mDate;
-    static int startDate;
-    static int startTime;
-    private RadioGroup mPriorityRadioGroup;
-    private RadioGroup mStatusRadioGroup;
-    private EditText mTitleText;
-    private RadioButton mDefaultStatusButton;
-    private RadioButton mDefaultPriorityButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
-        mTitleText = (EditText) findViewById(R.id.title);
-        mDefaultStatusButton = (RadioButton) findViewById(R.id.statusNotDone);
-        mDefaultPriorityButton = (RadioButton) findViewById(R.id.medPriority);
-        mPriorityRadioGroup = (RadioGroup) findViewById(R.id.priorityGroup);
-        mStatusRadioGroup = (RadioGroup) findViewById(R.id.statusGroup);
-        dateView = (TextView) findViewById(R.id.date);
-        timeView = (TextView) findViewById(R.id.time);
+        progres = (TextView)findViewById( R.id.progress );
+        progressBar = (ProgressBar)findViewById( R.id.progressbar );
+        somedata = (TextView)findViewById( R.id.datastring );
 
 
+        myTask = new Task(10);
+        myTimer = new Timer();
+        myTimer.schedule(myTask, 0, 1000);
 
-        setDefaultDateTime();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        final Button datePickerButton = (Button) findViewById(R.id.date_picker_button);
-        datePickerButton.setOnClickListener(new OnClickListener() {
-
+        mThread = new Thread( ) {
+            int progress;
             @Override
-            public void onClick(View v) {
-                showDatePickerDialog();
-            }
-        });
+            public void run() {
+                System.out.println("START");
+                progress = myTask.getProgress();
+                while (myTask.getProgress() <= 9){
 
-        final Button timePickerButton = (Button) findViewById(R.id.time_picker_button);
-        timePickerButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                showTimePickerDialog();
-            }
-        });
-
-        // OnClickListener for the Cancel Button,
-
-        final Button cancelButton = (Button) findViewById(R.id.cancelButton);
-        cancelButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setResult(RESULT_CANCELED);
-                finish();
-
-            }
-        });
-
-
-        final Button resetButton = (Button) findViewById(R.id.resetButton);
-        resetButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTitleText.setText("");
-                mStatusRadioGroup.check(mDefaultStatusButton.getId());
-                mPriorityRadioGroup.check(mDefaultPriorityButton.getId());
-                setDefaultDateTime();
-            }
-        });
-
-
-        final Button submitButton = (Button) findViewById(R.id.submitButton);
-        submitButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                Task.Priority priority = null;
-                switch (mPriorityRadioGroup.getCheckedRadioButtonId()){
-
-                    case R.id.lowPriority:
-                        priority = Task.Priority.LOW;
-                        break;
-
-                    case  R.id.medPriority:
-                        priority = Task.Priority.MED;
-                        break;
-
-                    case  R.id.highPriority:
-                        priority = Task.Priority.HIGH;
-                        break;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //adapter.notifyDataSetChanged();
+                            // dans l'idéal, séparer ce qui ce passe run on UI thread
+                            progres.setText( ""+myTask.getProgress());
+                            progressBar.setProgress( myTask.getProgress() );
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-
-
-                Task.Status status = null;
-
-                switch (mStatusRadioGroup.getCheckedRadioButtonId()){
-
-                    case R.id.statusDone:
-                        status = Task.Status.DONE;
-                        break;
-
-                    case R.id.statusNotDone:
-                        status = Task.Status.NOTDONE;
-                        break;
-
-                }
-
-
-                // Construct the Date string
-                String titleString = mTitleText.getText().toString();
-                String fullDate = dateString + " " + timeString;
-
-
-                long caldate= setProgress();
-                Calendar cc = Calendar.getInstance();
-                long setdate = cc.getTimeInMillis();
-
-
-
-                Toast.makeText( MainActivity.this, "package"+caldate+""+setdate, Toast.LENGTH_LONG ).show();
-
-
-                // Package ToDoItem data into an Intent
-                Intent data = new Intent();
-
-                Task.packageIntent(data, titleString, priority, status,
-                        fullDate, setdate);
-                setResult(RESULT_OK, data);
-                finish();
+                System.out.println("END!!!");
             }
-        });
+        };
+        mThread.start();
     }
-
-    // Do not modify below this point.
-
-    private void setDefaultDateTime() {
-
-        // Default is current time
-        mDate = new Date();
-        mDate = new Date(mDate.getTime());
-
-        Calendar c = Calendar.getInstance();
-        c.setTime(mDate);
-
-
-        setDateString(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
-                c.get(Calendar.DAY_OF_MONTH));
-
-        dateView.setText(dateString);
-
-        setTimeString(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),
-                c.get(Calendar.MILLISECOND));
-
-        timeView.setText(timeString);
-    }
-
-    private long setProgress () {
-        DatePicker datePicker = new DatePicker(this);
-        TimePicker timePicker = new TimePicker(this);
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
-        cal.set(Calendar.MONTH, datePicker.getMonth());
-        cal.set(Calendar.YEAR, datePicker.getYear());
-        cal.set(Calendar.HOUR, timePicker.getCurrentHour());
-        cal.set(Calendar.MINUTE, timePicker.getCurrentMinute());
-        long i= cal.getTimeInMillis();
-        return i;
-    }
-
-    private static void setDateString(int year, int monthOfYear, int dayOfMonth) {
-
-        // Increment monthOfYear for Calendar/Date -> Time Format setting
-        monthOfYear++;
-        String mon = "" + monthOfYear;
-        String day = "" + dayOfMonth;
-
-        if (monthOfYear < 10)
-            mon = "0" + monthOfYear;
-        if (dayOfMonth < 10)
-            day = "0" + dayOfMonth;
-
-        dateString = year + "-" + mon + "-" + day;
-    }
-
-    private static void setTimeString(int hourOfDay, int minute, int mili) {
-        String hour = "" + hourOfDay;
-        String min = "" + minute;
-
-        if (hourOfDay < 10)
-            hour = "0" + hourOfDay;
-        if (minute < 10)
-            min = "0" + minute;
-
-        timeString = hour + ":" + min + ":00";
-    }
-
-    private Task.Priority getPriority() {
-
-        switch (mPriorityRadioGroup.getCheckedRadioButtonId()) {
-            case R.id.lowPriority: {
-                return Task.Priority.LOW;
-            }
-            case R.id.highPriority: {
-                return Task.Priority.HIGH;
-            }
-            default: {
-                return Task.Priority.MED;
-            }
-        }
-    }
-
-    private Task.Status getStatus() {
-
-        switch (mStatusRadioGroup.getCheckedRadioButtonId()) {
-            case R.id.statusDone: {
-                return Task.Status.DONE;
-            }
-            default: {
-                return Task.Status.NOTDONE;
-            }
-        }
-    }
-
-    private String getToDoTitle() {
-        return mTitleText.getText().toString();
-    }
-
-
-    // DialogFragment used to pick a ToDoItem deadline date
-
-    public static class DatePickerFragment extends DialogFragment implements
-            DatePickerDialog.OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-            // Use the current date as the default date in the picker
-
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-
-
-
-        }
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            setDateString(year, monthOfYear, dayOfMonth);
-
-            dateView.setText(dateString);
-        }
-
-    }
-
-    // DialogFragment used to pick a ToDoItem deadline time
-
-    public static class TimePickerFragment extends DialogFragment implements
-            TimePickerDialog.OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return
-            return new TimePickerDialog(getActivity(), this, hour, minute, true);
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            setTimeString(hourOfDay, minute, 0);
-            timeView.setText(timeString);
-        }
-    }
-
-    private void showDatePickerDialog() {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
-    }
-
-    private void showTimePickerDialog() {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getFragmentManager(), "timePicker");
-    }
-
-
-
 }
--- app/src/main/java/organizer/chemgames/equilibrium/Task.java
+++ app/src/main/java/organizer/chemgames/equilibrium/Task.java
@@ -10,145 +10,31 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 public class Task extends TimerTask {
+        int i=0;
+        int mfin=0;
+        int progress;
+
+        Task (int fin){
+            this.mfin=fin;
+        }
+
+    public void run() {
+            if (i<mfin){
+                System.out.println("Time's up!"+i);
+                progress=i;
+                i++;
+            }else{
+                i = 0;
+            }
+        }
 
 
-
-    public enum Priority {
-        LOW, MED, HIGH
-    };
-
-    public enum Status {
-        NOTDONE, DONE
-    };
-
-    int i;
-    int z;
-    int progress = 0;
-
-//TODO: implémente runnable, lorsqu'on lance une thread, il a son compteur personnel
-
-    public final static String TITLE = "title";
-    public final static String PRIORITY = "priority";
-    public final static String STATUS = "status";
-    public final static String DATE = "date";
-    public final static String SETDATE = "set_date";
-    public final static SimpleDateFormat FORMAT = new SimpleDateFormat(
-            "yyyy-MM-dd HH:mm:ss", Locale.US);
-
-    private String mTitle = new String();
-    private Priority mPriority = Priority.LOW;
-    private Status mStatus = Status.NOTDONE;
-    private Date mDate = new Date();
-    private long mset_date = 0;
-
-
-    Task(String title, Priority priority, Status status, Date date, long set_date) {
-        this.mTitle = title;
-        this.mPriority = priority;
-        this.mStatus = status;
-        this.mDate = date;
-        this.mset_date = set_date;
-    }
-
-    Task(Intent intent) {
-
-        mTitle = intent.getStringExtra(Task.TITLE);
-        mPriority = Priority.valueOf(intent.getStringExtra(Task.PRIORITY));
-        mStatus = Status.valueOf(intent.getStringExtra(Task.STATUS));
-        mset_date = intent.getLongExtra(Task.SETDATE, 0 );
-
-        try {
-            mDate = Task.FORMAT.parse(intent.getStringExtra(Task.DATE));
-        } catch (ParseException e) {
-            mDate = new Date();
+        public int getProgress (){
+           return progress;
         }
     }
 
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
-    public Priority getPriority() {
-        return mPriority;
-    }
-
-    public void setPriority(Priority priority) {
-        mPriority = priority;
-    }
-
-    public Status getStatus() {
-        return mStatus;
-    }
-
-    public void setStatus(Status status) {
-        mStatus = status;
-    }
 
 
-    public Date getDate() {
-        return mDate;
-    }
-
-    public void setDate(Date date) {
-        mDate = date;
-    }
-
-
-    public long getSal_date() {
-        return mset_date;
-    }
-
-    public void setSet_date(int set_date) {
-        mset_date = set_date;
-    }
-
-
-
-                //affichage seulement
-
-                @Override
-                public void run() {
-                    //this repeats every 1000 ms
-                    long scheduled_time = mDate.getTime();
-                    String aat= String.valueOf(scheduled_time);
-                    double st =  Double.parseDouble(aat);
-                    long when_scheduled = mset_date;
-                    String sst= String.valueOf(when_scheduled);
-                    double ws =  Double.parseDouble(sst);
-
-                    double b = st-ws; //scheduled - when scheduled
-                    final double prog = b/1000;
-                    if (i<100 ){
-                        z = ( (int) (100 / prog * i) );
-                        i++; }
-                }
-
-                public int getTimerProgress(){
-                return z;
-                }
-
-
-
-
-
-
-
-
-    public static void packageIntent(Intent intent, String title,
-                                     Priority priority, Status status, String date,  long set_date) {
-
-        intent.putExtra(Task.TITLE, title);
-        intent.putExtra(Task.PRIORITY, priority.toString());
-        intent.putExtra(Task.STATUS, status.toString());
-        intent.putExtra(Task.DATE, date);
-        intent.putExtra(Task.SETDATE, set_date);
-
-
-    }
-
-}
